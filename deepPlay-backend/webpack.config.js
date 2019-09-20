84c2773f3dd7b13aa2880e87a3a88bc314338a0e
Sonu <sonu.chapter247@gmail.com>
2019-09-20 14:07:56 +0530

    Webpack added for better fitleration

---
+++ deepPlay-backend/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const { NODE_ENV = "production" } = process.env;
+const nodeExternals = require("webpack-node-externals");
+var CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: "./app/app.ts",
+  mode: NODE_ENV,
+  externals: [nodeExternals()],
+  target: "node",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "app.js"
+  },
+  resolve: {
+    extensions: [".ts", ".js", ".jsx", ".tsx"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: ["ts-loader"]
+      }
+    ]
+  },
+  plugins: [new CopyWebpackPlugin([{ from: "app/public", to: "public" }])]
+};
