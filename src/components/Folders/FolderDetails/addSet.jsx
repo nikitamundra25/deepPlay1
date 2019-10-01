30ac5057bc1d6990d1c2b7e8ea05afecbff09840
rohit-chapter <rohit.chapter247@gmail.com>
2019-10-01 11:16:18 +0530

    add set modal design

--- src/components/Folders/FolderDetails/addSet.jsx
+++ src/components/Folders/FolderDetails/addSet.jsx
@@ -1,7 +1,17 @@
 import React from "react";
-import { Modal, ModalBody, ModalHeader, Row, Col, Button } from "reactstrap";
+import {
+  Modal,
+  ModalBody,
+  ModalHeader,
+  Row,
+  Col,
+  Button,
+  FormGroup,
+  Input
+} from "reactstrap";
 import { AppRoutes } from "../../../config/AppRoutes";
 import closeIcon from "../../../assets/img/close-img.png";
+import "./index.scss";
 
 // core components
 class AddSetModal extends React.Component {
@@ -67,30 +77,49 @@ class AddSetModal extends React.Component {
           </ModalHeader>
           <ModalBody className="modal-text-center">
             <div className="wrap-folder">
-              <Button
-                color=" "
-                onClick={() => this.handleAddNewSet(folderId)}
-                className={"cursor_pointer btn btn-black"}
-              >
-                + Create New Set
-              </Button>
+              <div className="tile-wrap card mb-3 d-block">
+                <span
+                  onClick={() => this.handleAddNewSet(folderId)}
+                  className={"cursor_pointer create-btn font-14 text-uppercase"}
+                >
+                  + Create New Set
+                </span>
+              </div>
+              <FormGroup>
+                <Input
+                  type="select"
+                  name="select"
+                  id="exampleSelect"
+                  className="rounded-0"
+                >
+                  <option>Your Sets</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                </Input>
+              </FormGroup>
               {setList && setList.length
                 ? setList.map((set, i) => {
                     return (
                       <Row className="set-wrap" key={i}>
                         <Col md="12">
                           <div className="tile-wrap card">
-                            <div className="d-flex flaex-warp">
-                              <div className="sets-heading"> {set.title}</div>
-                              <div className="sets-icon ">
+                            <div className="d-flex flex-warp align-item-center">
+                              <div className="sets-heading text-capitalize">
+                                {" "}
+                                {set.title}
+                              </div>
+                              <div className="sets-icon ml-auto">
                                 {set.folderId !== null ? (
                                   <span
                                     onClick={() =>
                                       this.OnhandleSets(set._id, "add")
                                     }
+                                    className="text-center cursor_pointer"
                                   >
                                     <i
-                                      className="fa fa-plus-square-o"
+                                      className="fa fa-minus"
                                       aria-hidden="true"
                                     ></i>
                                   </span>
@@ -99,9 +128,10 @@ class AddSetModal extends React.Component {
                                     onClick={() =>
                                       this.OnhandleSets(set._id, "remove")
                                     }
+                                    className="text-center cursor_pointer "
                                   >
                                     <i
-                                      className="fa fa-minus"
+                                      className="fa fa-plus"
                                       aria-hidden="true"
                                     ></i>
                                   </span>
