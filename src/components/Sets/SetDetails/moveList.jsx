ccb370724b02116ca54b8451a3cdba22c6158a10
Rishabh Bula <rishab.chapter247@gmail.com>
2019-10-14 17:28:25 +0530

    major changes in moves list section

--- src/components/Sets/SetDetails/moveList.jsx
+++ src/components/Sets/SetDetails/moveList.jsx
@@ -21,6 +21,8 @@ class MoveList extends React.Component {
       videoIndex: -1,
       isVideoChecked: false,
       isSelectVideo: false,
+      selectedMoves: [],
+      selectedMoveIds: [],
       page: 1
     };
   }
@@ -39,14 +41,46 @@ class MoveList extends React.Component {
   }
   /* 
   */
-  handleVideoCheckBox = (index) => {
+  handleVideoCheckBox = (checked, index, moveId) => {
+    const selectedMoves = [...this.state.selectedMoves]
+    selectedMoves[index] = checked
+    let selectedMoveIds = [...this.state.selectedMoveIds]
+    selectedMoveIds.push(moveId)
     this.setState({
       isVideoChecked: true,
-
+      selectedMoves,
+      selectedMoveIds
     })
   }
   /* 
   */
+  handleMovesSelect = (valueCheck, e, index, moveId) => {
+    let checked
+    if (e && e.target) {
+      checked = !e.target.checked
+    } else {
+      checked = valueCheck
+    }
+    const selectedMoves = [...this.state.selectedMoves]
+    selectedMoves[index] = checked
+    let selectedMoveIds = [...this.state.selectedMoveIds]
+    if (checked === true) {
+      selectedMoveIds.push(moveId)
+    } else {
+      selectedMoveIds = selectedMoveIds.filter(item => item !== moveId)
+    }
+    if (!selectedMoveIds.length && selectedMoves && selectedMoves.length) {
+      this.setState({
+        isVideoChecked: false
+      })
+    }
+    this.setState({
+      selectedMoves,
+      selectedMoveIds
+    })
+  }
+  /* 
+   */
   handleVideoPlay = (index) => {
     let myVideo = document.getElementById(`webm-video-${index}`);
     myVideo.play();
@@ -61,7 +95,7 @@ class MoveList extends React.Component {
   */
   render() {
     const { show, setIndex, moveCount, movesOfSet } = this.props
-    const { isVideoChecked, isSelectVideo, videoIndex } = this.state
+    const { isVideoChecked, isSelectVideo, videoIndex, selectedMoves, selectedMoveIds } = this.state
     return (
       <section className="play-list-collection set-detail-section">
         <Row>
@@ -69,6 +103,36 @@ class MoveList extends React.Component {
             <div class="content-header mt-3 mb-1">
               <span class="content-title">Moves in this set ({moveCount || 0})</span>
             </div>
+            {
+              selectedMoveIds && selectedMoveIds.length ?
+                <div className={"selected-moves"}>
+                  <div className={"d-flex justify-content-between"}>
+                    <div class="content-title">
+                      Selected Moves: {selectedMoveIds && selectedMoveIds.length ? selectedMoveIds.length : 0}
+                    </div>
+                    <div class="content-title pr-2">
+                      <span className={"mr-2"}>
+                        <ButtonGroup
+                          size="sm">
+                          <Button>Copy</Button>
+                          <Button>Transfer</Button>
+                          <Button>Remove</Button>
+                        </ButtonGroup>
+                      </span>
+                      <span
+                        onClick={() => this.setState({
+                          selectedMoves: [],
+                          selectedMoveIds: [],
+                          isVideoChecked: false
+                        })}
+                        className={"cursor_pointer"}>
+                        <i class="fa fa-times" aria-hidden="true" />
+                      </span>
+                    </div>
+                  </div>
+                </div> :
+                null
+            }
           </Col>
           <div className="play-list-tile" >
             <div className="play-list-block  d-flex h-100 ">
@@ -76,7 +140,10 @@ class MoveList extends React.Component {
                 <div className="h5 font-dark-bold add-img">
                   <img src={addPlusIc} alt="" />
                 </div>
-                <Button onClick={() => this.props.redirectTo(AppRoutes.MOVE.url)} color={" "} className="fill-btn btn mt-4">
+                <Button
+                  onClick={() => this.props.redirectTo(AppRoutes.MOVE.url)}
+                  color={" "}
+                  className="fill-btn btn mt-4">
                   {" "}
                   Create Now
                 </Button>
@@ -99,10 +166,13 @@ class MoveList extends React.Component {
                     onMouseLeave={() => {
                       this.handleVideoPause(index);
                     }}>
-                    <div onMouseOver={() => this.handleVideoPlay(index)} className="play-list-img blur-img-wrap checked-wrap">
+                    <div
+                      onMouseOver={() => this.handleVideoPlay(index)}
+                      onClick={isVideoChecked ? () => this.handleMovesSelect(!selectedMoves[index], null, index, video._id) : null}
+                      className={isVideoChecked && selectedMoves[index] ? `play-list-img blur-img-wrap checked-wrap video-select` : `play-list-img blur-img-wrap checked-wrap`}>
                       {
                         !isVideoChecked && isSelectVideo && (videoIndex === index) ?
-                          <span onClick={() => this.handleVideoCheckBox(index)} className="plus-ic-wrap">
+                          <span onClick={() => this.handleVideoCheckBox(true, index, video._id)} className="plus-ic-wrap">
                             <i className="text-white fa fa-plus-circle" aria-hidden="true" />
                           </span> :
                           null
@@ -112,10 +182,13 @@ class MoveList extends React.Component {
                           <span className="plus-ic-wrap custom-control custom-checkbox">
                             <Input
                               className="custom-control-input"
-                              id="customCheck1"
+                              id={`selected-video-${index}`}
+                              onChange={(e) => this.handleMovesSelect(null, e, index, video._id)}
                               type="checkbox"
+                              checked={selectedMoves[index] ? true : false}
+
                             />
-                            <label className="custom-control-label" htmlFor="customCheck1" />
+                            <label className="custom-control-label" htmlFor={`selected-video-${index}`} />
                           </span> :
                           null
                       }
