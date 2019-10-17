d59e13b648582283c771ba1b5a29f8f8f45dd021
Rishabh Bula <rishab.chapter247@gmail.com>
2019-10-17 15:42:21 +0530

    minor change

--- src/components/Page404/index.jsx
+++ src/components/Page404/index.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Col, Container, Input, InputGroup, InputGroupAddon, InputGroupText, Card, Row } from 'reactstrap';
+import { Button, Col, Container, Card, Row } from 'reactstrap';
 import "./index.scss";
 import imgNotfound from "../../assets/img/404.svg"
 class Page404Component extends Component {
@@ -20,7 +20,7 @@ class Page404Component extends Component {
             <Col md="6">
               {/* <iframe width="560" title={"Dance"} height="315" src="https://www.youtube.com/embed/nrDtcsyd-U4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */}
               <div className="d-flex video-add-banner justify-content-center align-items-center">
-             <img src={imgNotfound} className="w-100"/>
+             <img src={imgNotfound} alt={""} className="w-100"/>
               </div>
             </Col>
             <Col md="6" className="d-flex flex-column justify-content-between ">
