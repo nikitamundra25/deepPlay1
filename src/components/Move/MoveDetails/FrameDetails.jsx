a6ce8822d2a300fb940f54e20786ae320f31a8b4
sohal khan <sohail.chapter247@gmail.com>
2019-11-04 12:19:40 +0530

    some changes

--- src/components/Move/MoveDetails/FrameDetails.jsx
+++ src/components/Move/MoveDetails/FrameDetails.jsx
@@ -216,19 +216,20 @@ class FrameDetails extends Component {
             md={"6"}
             className="text-right d-flex align-items-end justify-content-end"
           >
-            <Button
+           <Button
               color={"default"}
               className={"btn-line-black btn url-upload-btn mr-3"}
-          
+              onClick={() => {
+                window.history.back();
+              }}
             >
             Back
             </Button>
             <Button
               color={"default"}
               className={"btn-black btn url-upload-btn"}
-              onClick={() => {
-                window.history.back();
-              }}
+            
+              onClick={()=>this.props.completeEditing()}
             >
               Finish
             </Button>
