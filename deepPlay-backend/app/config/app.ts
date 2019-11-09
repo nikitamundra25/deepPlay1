ef6d0d41c3d639445c25ad4a19a3319d582ca0b0
RishabhBula1712 <48308063+RishabhBula1712@users.noreply.github.com>
2019-11-09 14:15:40 +0530

    Update app.ts

--- deepPlay-backend/app/config/app.ts
+++ deepPlay-backend/app/config/app.ts
@@ -1,7 +1,7 @@
 export const IsProductionMode = process.env.NODE_ENV !== "development";
 export const webURL = !IsProductionMode
   ? "localhost:3001"
-  : "http://54.85.183.188:8009";
+  : "http://192.249.123.124:8005";
 export const CloudinaryAPIKey = IsProductionMode
   ? "955984653579861"
   : "955984653579861";
@@ -15,7 +15,7 @@ export const algoliaAPIKey = IsProductionMode
   : "67d8975eec42c620cf98a1f3acb517f3";
 export const ServerURL = !IsProductionMode
   ? "http://192.168.2.120:8000"
-  : "http://54.85.183.188";
+  : "http://192.249.123.124";
 export const s3Key = {
   keyId: "AKIA5R6U3BHKVS3EFDGW",
   key: "hRZ8bTRNBsxThak7Dmf8lep7q7whcSRlRiFX/1OQ",
