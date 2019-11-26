64ff892671d457c329f1758a8332c47f6d87c790
ayushij.chapter247@gmail.com <nikitam.chapter247@gmail.com>
2019-11-26 15:32:02 +0530

    Minor change

--- src/components/Sets/SetDetails/WebmView.jsx
+++ src/components/Sets/SetDetails/WebmView.jsx
@@ -93,7 +93,7 @@ class WebmView extends Component {
 
         if (this.props.isFullScreenMode && videoFullScreen) {
           let isVideoScreenChange = false;
-          this.video.addEventListener("webkitfullscreenchange", () => {
+          this.video.addEventListener("webkitfullscreenchange ", () => {
             this.setState({
               isFullScreenMode: false
             });
