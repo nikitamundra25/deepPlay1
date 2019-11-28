085ff6746ad8e2a0f589428c885ba05d301874af
RishabhBula1712 <48308063+RishabhBula1712@users.noreply.github.com>
2019-11-28 10:38:10 +0530

    Update moves.ts

--- deepPlay-backend/app/controllers/moves.ts
+++ deepPlay-backend/app/controllers/moves.ts
@@ -204,7 +204,7 @@ const getVideoFrames = async (videoName: string): Promise<any> => {
       `${dirName.split(".")[0]}_frames`,
       {
         start_time: 0,
-         every_n_percentage: 10
+        frame_rate: 20 / videoDuration
       },
       (error: any, file: any) => {
         if (error) {
