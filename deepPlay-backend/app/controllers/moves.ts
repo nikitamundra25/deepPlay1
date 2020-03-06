e2b888fc47249d95b7b2c145a68d086527ed47c3
Rishabh Bula <rishab.chapter247@gmail.com>
2020-03-06 19:46:56 +0530

    major changes

--- deepPlay-backend/app/controllers/moves.ts
+++ deepPlay-backend/app/controllers/moves.ts
@@ -589,7 +589,7 @@ const updateMoveDetailsFromYouTubeAndTrim = async (
       );
     }
     let videoStream: any;
-    const video = youtubedl(result.sourceUrl, ["--format= 18"], {});
+    const video = youtubedl(result.sourceUrl, ["--format= 137"], {});
     // video.on('info', function(info) {
     //   console.log('Download started')
     //   console.log('filename: ' + info._filename)
