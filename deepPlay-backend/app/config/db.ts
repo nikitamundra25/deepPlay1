21e938b2847c948dba30cc3dfac6ae9cf087e8e3
Sonu <sonu@chapter247.com>
2020-03-31 17:42:19 +0530

    Fixed YouTube Video URL

--- deepPlay-backend/app/config/db.ts
+++ deepPlay-backend/app/config/db.ts
@@ -1,6 +1,8 @@
 import { IsProductionMode } from "./app";
 
-export const DBProtocol: string = IsProductionMode ? "mongodb" : "mongodb";
-export const DBHost: string = IsProductionMode ? "localhost" : "localhost";
-export const DBName: string = IsProductionMode ? "deepPlay" : "deepPlay";
-export const Port: number = !IsProductionMode ? 8000 : 8005 
+export const DBProtocol: string = IsProductionMode ? "mongodb+srv" : "mongodb";
+export const DBHost: string = IsProductionMode
+  ? "deepplay_web:qrF2CGsArsSG4R3K@deepplay-pprn4.mongodb.net"
+  : "localhost";
+export const DBName: string = IsProductionMode ? "deepplay_web" : "deepPlay";
+export const Port: number = !IsProductionMode ? 8000 : 8005;
